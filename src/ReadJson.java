a1c149c7e484a311b497943bc45b2d222566d1b0
Jbee26 <jack_belyea26@milton.edu>
2024-11-15 13:03:56 -0500

    :)

--- src/ReadJson.java
+++ src/ReadJson.java
@@ -91,27 +91,61 @@ public class ReadJson {
             //JSONArray starships = (JSONArray)jsonObject.get("starships");
 
 
+
             org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObject.get("abilities");
+            org.json.simple.JSONArray msg2 = (org.json.simple.JSONArray) jsonObject.get("forms");
+            org.json.simple.JSONArray msg3 = (org.json.simple.JSONArray) jsonObject.get("game_indices");
+
             int n =   msg.size(); //(msg).length();
+            int n2 = msg2.size();
+            int n3 = msg2.size();
+
             for (int i = 0; i < n; ++i) {
                 JSONObject test = (JSONObject) msg.get(i);
                 System.out.println(test);
                 // System.out.println(person.getInt("key"));
-                JSONObject name = (JSONObject) test.get("ability");
-                System.out.println(name);
-                String imposter = (String) name.get("name");
-                System.out.println(imposter);
+                JSONObject name1 = (JSONObject) test.get("ability");
+                System.out.println(name1);
+                String abName = (String) name1.get("name");
+                System.out.println(abName);
+            }
+
+            for (int a = 0; a < n2; ++a) {
+                JSONObject test2 = (JSONObject) msg2.get(a);
+                System.out.println(test2);
+                String title = (String) test2.get("name");
+                System.out.println(title);
+//
+            }
 
 
-
-
-
+            for (int l = 0; l < n3; ++l) {
+                JSONObject test3 = (JSONObject) msg3.get(l);
+                System.out.println(test3);
+                JSONObject version = (JSONObject) test3.get("version");
+                System.out.println(version);
+                String vName = (String) version.get("name");
+                System.out.println(vName);
             }
 
 
 
 
-           // System.out.println(mass);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // System.out.println(mass);
           //  System.out.println(eColor);
            // System.out.println(bYear);
           //  for (int i = 0; i < starships.size(); i++) {
